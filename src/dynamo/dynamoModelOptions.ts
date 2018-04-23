f1dfa3ea4c5546d70106f1e942bc79d230d9c2e1
Dumitru Cantea <canteadumitru@gmail.com>
2018-04-23 18:02:03 +0300

    deleted dynamodb models

--- src/dynamo/dynamoModelOptions.ts
+++
@@ -1,9 +0,0 @@
-
-export interface DynamoModelOptions {
-    name: string
-    tableName: string
-    hashKey: string
-    rangeKey?: string
-    schema: any
-    indexes?: { hashKey?: string, rangeKey?: string, type: 'local' | 'global', name: string, projection?: any }[]
-}
