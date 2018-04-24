f5c0646c1f074003f3d42cfdf4697f3118404dfd
Dumitru Cantea <canteadumitru@gmail.com>
2018-04-24 11:31:25 +0300

    module export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 
+export { createConnection } from 'mongoose';
+
+export { ConceptModel } from './mongo/conceptModel';
+export { ConceptRootNameModel } from './mongo/conceptRootNameModel';
+export { WikiSearchNameModel } from './mongo/wikiSearchNameModel';
+export { WikiTitleModel } from './mongo/wikiTitleModel';
+
 export { ConceptRepository } from './conceptRepository';
 export { ConceptRootNameRepository } from './conceptRootNameRepository';
 export { WikiSearchNameRepository } from './wikiSearchNameRepository';
